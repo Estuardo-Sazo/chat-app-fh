b88403a6038f342d66cc375eae2d3e0dec3e8b24
Estuardo-Sazo <estuardosazo1206@gmail.com>
2024-06-14 22:30:41 -0600

    Caja de texto de nuestro chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -36,11 +44,60 @@ class ChatPage extends StatelessWidget {
             //TODO CAJA DE TEXTO
             Container(
               color: Colors.white,
-              height: 50,
+              child: _inputChat(),
             )
           ],
         ),
       )
     );
+  }
+
+  Widget _inputChat() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: SafeArea(
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                onChanged: (String text){
+                  //TODO Cuando hay un valor para enviar
+
+
+                  setState(() {
+                    
+                  });
+                },
+                decoration: const InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje'
+                ),
+                focusNode: _focusNode,
+              ),
+            ),
+            //Boton de enviar
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              child: IconTheme(
+                data: IconThemeData(color: Colors.blue[400]),
+                child: IconButton(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  icon: const Icon(Icons.send),
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+
+  _handleSubmitted(String text) {
+    print(text);
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
